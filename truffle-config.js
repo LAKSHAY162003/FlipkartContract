b1f093122219b3c2984252a8ef6cc650c50de806
LAKSHAY162003 <lakshay.sharma.ug21@nsut.ac.in>
2023-08-17 03:38:44 +0530

    Some Changes in Config Files

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const INFURA_API_KEY = "https://sepolia.infura.io/v3/ad9cad8dc5564c1cac9f7f6a30d5de04";
+const INFURA_API_KEY = "https://sepolia.infura.io/v3/4bc5c07aca6d4360a4947ea517a585dd";
 
 const mnemonic = "when various mask cool practice version spot mobile point ball space apology";
 
